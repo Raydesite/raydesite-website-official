559ab240a8e277676cbd5cd99a1b94b43a34a677
Yulianny Betancourt <flipsdulce@gmail.com>
2026-05-06 22:50:42 -0400

    fix:update title - opengraph-image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -183,7 +183,8 @@ export default async function Image() {
 								letterSpacing: '-0.03em',
 							}}
 						>
-							Software built <span style={{ color: '#F5A623' }}> to last.</span>
+							Software built&nbsp;
+							<span style={{ color: '#F5A623' }}>to last.</span>
 						</span>
 						<span
 							style={{
@@ -195,7 +196,7 @@ export default async function Image() {
 								marginTop: '8px',
 							}}
 						>
-							Businesses <span style={{ color: '#F5A623' }}> built</span> to
+							Businesses&nbsp;<span style={{ color: '#F5A623' }}>built</span> to
 							scale.
 						</span>
 					</div>
